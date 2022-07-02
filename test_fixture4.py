e1566f2e8ccda1be48de56688ab7cc49405cba83
Serg <89090168690@mail.ru>
2022-07-02 21:44:55 +0500

    Add file домашка fixture3 добавил принты + fixture4

--- test_fixture4.py
+++ test_fixture4.py
@@ -5,12 +5,11 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-@pytest.fixture
+@pytest.fixture(scope="class")
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
     yield browser
-    # этот код выполнится после завершения теста
     print("\nquit browser..")
     browser.quit()
 
@@ -18,13 +17,13 @@ def browser():
 class TestMainPage1:
     # вызываем фикстуру в тесте, передав ее как параметр
     def test_guest_should_see_login_link(self, browser):
-        print("\ntest 1 start")
+        print("start test1")
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, "#login_link")
-        print("\ntest 1 finish")
+        print("finish test1")
 
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
-        print("\ntest 2 start")
+        print("start test2")
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
-        print("\ntest 2 finish")
+        print("finish test2")
